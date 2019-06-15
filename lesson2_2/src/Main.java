c1f9a269182c24ff66116142cc8b582322367900
Pashok94 <pavel.kuzmin.1994@mail.ru>
2019-06-15 13:54:09 +0300

    урок 2_2

--- lesson2_2/src/Main.java
+++ lesson2_2/src/Main.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
@@ -30,7 +27,7 @@ public class Main {
 
     public static void insertStudent (String first_name, String last_name, int group_number){
         String sql = String.format("INSERT INTO student_list (first_name, last_name, group_numb)" +
-                "values('%s','%s',%d", first_name,last_name,group_number);
+                "values('%s','%s',%d);", first_name,last_name,group_number);
         try {
             stmt.execute(sql);
         } catch (SQLException e) {
@@ -38,9 +35,37 @@ public class Main {
         }
     }
      public static void createTable (String name, String column1, String column2){
-        String sql = String.format("CREATE TABLE %s (%s TEXT, %s TEXT", name,column1,column2);
+        String sql = String.format("CREATE TABLE %s (%s TEXT, %s TEXT);", name,column1,column2);
          try {
              stmt.execute(sql);
+         } catch (SQLException e) {
+             e.printStackTrace();
+         }
+     }
+
+     public static void delete (int id){
+        String sql = String.format("DELETE FROM student_list WHERE id = %d;", id);
+         try {
+             stmt.execute(sql);
+         } catch (SQLException e) {
+             e.printStackTrace();
+         }
+     }
+
+     public static void update (int id, int newGroupNumb){
+        String sql = String.format("UPDATE student_list SET group_numb = %d WHERE id = %d;", newGroupNumb, id);
+         try {
+             stmt.execute(sql);
+         } catch (SQLException e) {
+             e.printStackTrace();
+         }
+     }
+
+     public static void select (int id){
+        String sql = String.format("SELECT * FROM student_list WHERE id = %d;", id);
+         try {
+             ResultSet rs = stmt.executeQuery(sql);
+             System.out.println(rs.getString(2) + " " + rs.getString(3) + " " + rs.getString(4) );
          } catch (SQLException e) {
              e.printStackTrace();
          }
--- lesson2_2/src/SQLTest.java
+++ lesson2_2/src/SQLTest.java
@@ -4,8 +4,11 @@ public class SQLTest {
     public static void main(String[] args) {
         try {
             Main.connect();
-            Main.insertStudent("Vasiliy", "Pupkin", 130);
-            //Main.createTable("newTable", "column1", "column2");
+//            Main.insertStudent("Nikola", "Travkin", 125);
+//            Main.createTable("newTable", "column1", "column2");
+//            Main.delete(1);
+//            Main.update(2, 125);
+            Main.select(2);
         } catch (SQLException e) {
             e.printStackTrace();
         }
